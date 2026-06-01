61937986975ef23c8ce97e46636f02444a4f7626
ul1 <ulwan0911@gmail.com>
2026-06-01 14:24:09 +0700

    better look

--- Assets/Scripts/Player/PlayerAnimationTrigger.cs
+++ Assets/Scripts/Player/PlayerAnimationTrigger.cs
@@ -13,9 +13,14 @@ public class PlayerAnimationTrigger : MonoBehaviour
         //     Vector3.zero
         // );
         
+        Vector3 d = controller.Pointer.position - controller.transform.position;
+        float angle = Mathf.Atan2(d.z, d.x) * Mathf.Rad2Deg;
+        if (angle < 0) angle += 360.0f;
+
         attackVFX.SetActive(true);
         attackVFX.transform.position = transform.position + new Vector3(0, heightMod, 0) + (transform.forward * 1.25f);
-        attackVFX.transform.LookAt(new Vector3(controller.Pointer.position.x, 0.5f, controller.Pointer.position.z));
+        attackVFX.transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
         attackVFX.GetComponent<DamageComponent>().damage = PlayerStats.instances.damage;
+        // attackVFX.transform.LookAt(new Vector3(controller.Pointer.position.x, 0.5f, controller.Pointer.position.z));
     }
 }
